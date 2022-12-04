5c213b6aca5d83f152cd33c2ab45f9ac74816a25
baonny <baonnyhui11@gmail.com>
2022-12-03 21:40:30 -0500

    updated ghost move

--- ghost.py
+++ ghost.py
@@ -5,9 +5,30 @@ class Ghost:
         self.position = starting_position
         self.level = level
 
-    def move(self, possible_actions):
+    def move(self):
         # Get possible actions and then pick a random action (random policy)
+        north = [self.position[0] - 1, self.position[1]]
+        south = [self.position[0] + 1, self.position[1]]
+        east = [self.position[0], self.position[1] + 1]
+        west = [self.position[0], self.position[1] - 1]
         
-        probability = random.randrange(len(possible_actions))
+        possible_actions = []
 
+        walls = self.level.get_wall_coordinates()
+
+        # North
+        if north[0] > 0 and north not in walls:
+            possible_actions.append(north)
+        # South
+        if south[0] < level.height and south not in walls:
+            possible_actions.append(south)
+        # East
+        if east[1] < level.width and east not in walls:
+            possible_actions.append(east)
+        # West
+        if west[1] > 0 and west not in walls:
+            possible_actions.append(west)
+
+        probability = random.randrange(len(possible_actions))
+        
         self.position = possible_actions[probability]
