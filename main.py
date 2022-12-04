e83bb3ba17cafdd170dfed309a45b36c57e8361c
18ajw15 <18ajw15@queensu.ca>
2022-12-03 19:29:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -29,45 +29,39 @@ def get_possible_actions(position, level):
 
 # Can change the parameters of collision() and next() to not have pacman, ghost1, etc.
 
-def collision(pacman, ghost1, ghost2, ghost3):
-    if pacman.position == ghost1.position:
-        return True
-    elif pacman.position == ghost2.position:
-        return True
-    elif pacman.position == ghost3.position:
-        return True
+def collision(pacman, ghosts):
+    for ghost in ghosts:
+        if pacman.position == ghost.position:
+          return True
     return False
 
 
-def next(pacman, ghost1, ghost2, ghost3):
+def next(pacman, ghosts):
     pacman.move(get_possible_actions(pacman.position)) # Need to  change the move function depending on how we do the qlearning
-    if (collision(pacman, ghost1, ghost2, ghost3)):
-        return True
-
-    ghost1.move(get_possible_actions(ghost1.position))
-    if (collision(pacman, ghost1, ghost2, ghost3)):
+    if (collision(pacman, ghosts)):
         return True
     
-    ghost2.move(get_possible_actions(ghost2.position))
-    if (collision(pacman, ghost1, ghost2, ghost3)):
-        return True
-
-    ghost3.move(get_possible_actions(ghost3.position))
-    if (collision(pacman, ghost1, ghost2, ghost3)):
+    for ghost in ghosts:
+        ghost.move(get_possible_actions(ghost.position))
+    if (collision(pacman, ghosts)):
         return True
     
     return False
+
 # Initialize level
 game_over = False
 level = DefaultLevel()
 
 # Initialize Pacman
-pacman = Pacman("Starting Position", level)
+pacman = Pacman([22,12], level)
 
 # Initialize Ghosts
-ghost1 = Ghost("Starting Position 1", level)
-ghost2 = Ghost("Starting Position 2", level)
-ghost3 = Ghost("Starting Position 3", level)
+ghost1 = Ghost([10,11], level)
+ghost2 = Ghost([10,12], level)
+ghost3 = Ghost([10,13], level)
+ghost4 = Ghost([10,14], level)
+
+ghosts = [ghost1, ghost2, ghost3, ghost4]
 
 while not game_over:
     if (next()):
