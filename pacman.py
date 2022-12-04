31904829340732451b0cd03a29b8f0fbe9a2778e
bentomkinson2000 <92761852+bentomkinson2000@users.noreply.github.com>
2022-12-03 21:41:53 -0500

    Update pacman.py

--- pacman.py
+++ pacman.py
@@ -5,25 +5,8 @@ class Pacman:
     def __init__(self, starting_position, level, possibleActions):
         self.position = starting_position
         self.level = level
+        #This is entered my calling the get_possible_actions function in main as the variable
         self.possibleMoves = possibleActions
-   
-    def eGreedy(self,grid, epsilon):
-
-        #Two lines gets the max value found in qtable for specific state
-        position = grid[(self.position[0], self.position[1])]
-        bestAction = max(position, key=position.get)
-
-        #Adds the probability for each action in a list, the one that is best gets the highest probability
-        probability = []
-        for i in self.possibleMoves:
-            if i == bestAction:
-                probability.append((epsilon  / len(self.possibleMoves))) + 1 - epsilon)
-            else:
-                probability.append(epsilon / len(self.possibleMoves))
-        
-        #get the a random value with non uniform probability
-        nonUniform = np.random.choice(self.possibleMoves, size = 1, p = probability)
-        return nonUniform.item()
 
     def getPossibleActions(self, actions):
         possibleActions = []
